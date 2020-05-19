363481cf37a5825672b9d37dc57fbea1b99723af
MarcosVenturo <65518116+MarcosVenturo@users.noreply.github.com>
2020-05-18 21:49:16 -0500

    Update Persona.h

--- Persona.h
+++ Persona.h
@@ -13,11 +13,11 @@ namespace VentiladorMecanicoModel
 		String^ sexo;
 		float estatura;
 		String^ fechaIngreso;
-		String^ fechaSalida;
+		String^ horaIntubacion;
 		int edad;
 		String^ horaEntrada;
 
 	public:
-		Persona(String^ nombre, String^ apellido, String^ DNI, String^ sexo, float estatura, String^ fechaIngreso, String^ fechaSalida, int edad, String^ horaEntrada);
+		Persona(String^ nombre, String^ apellido, String^ DNI, String^ sexo, float estatura, String^ fechaIngreso, String^ horaIntubacion, int edad, String^ horaEntrada);
 	};
 }
