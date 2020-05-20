d23d14acab13747c433bf8875d412376b315c8c5
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-20 17:01:08 -0500

    Add files via upload

---
+++ VolumenControl.cpp
@@ -0,0 +1,8 @@
+#include "VolumenControl.h"
+
+using namespace VentiladorMecanicoModel;
+
+VolumenControl::VolumenControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float volumenCorriente, float alarmaPresionMax) {
+	this-> volumenCorriente;
+	this-> alarmaPresionMax;
+}
---
+++ VolumenControl.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Mandatorio.h>
+
+namespace VentiladorMecanicoModel
+{
+	using namespace System;
+	public ref class VolumenControl :public Mandatorio 
+	{
+	private:
+		float volumenCorriente;
+		float alarmaPresionMax;
+	public:
+		VolumenControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float volumenCorriente, float alarmaPresionMax );
+	};
+}
