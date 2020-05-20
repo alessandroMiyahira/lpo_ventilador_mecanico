064228854f5511d7bfb7c8c40f5e4f4e52cf07ff
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-20 17:07:54 -0500

    Update PresionControl.h

--- PresionControl.h
+++ PresionControl.h
@@ -1,10 +1,11 @@
 #pragma once
+#include "Mandatorio.h"
 
 namespace VentiladorMecanicoModel{
 	
 	using namespace System;
 	
-	public ref class PresionControl{
+	public ref class PresionControl : public Mandatorio {
 	
 	public:
 		PresionControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float alarmaVolMin, float alarmaVolMax, float PCsobrePEEP);
