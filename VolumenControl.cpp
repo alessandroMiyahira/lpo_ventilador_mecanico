1e13796ef417248e021afe60639e3a1fec6ea816
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-21 09:18:33 -0500

    Update VolumenControl.cpp

--- VolumenControl.cpp
+++ VolumenControl.cpp
@@ -2,7 +2,7 @@
 
 using namespace VentiladorMecanicoModel;
 
-VolumenControl::VolumenControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float volumenCorriente, float alarmaPresionMax) {
+VolumenControl::VolumenControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float volumenCorriente, float alarmaPresionMax):Mandatorio( PEEP, concentracionO2, retardoInspiratorio,complacencia, resistenciaAlveolar,tiempoInspiracion,triggerFlujo,frecuencia) {
 	this-> volumenCorriente;
 	this-> alarmaPresionMax;
 }
