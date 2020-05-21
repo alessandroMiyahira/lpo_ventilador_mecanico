ff5c740a6de2363df95e2dd9bca89039b5c47c42
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-21 09:19:23 -0500

    Update PresionControl.cpp

--- PresionControl.cpp
+++ PresionControl.cpp
@@ -2,7 +2,7 @@
 
 using namespace VentiladorMecanicoModel;
 
-PresionControl::PresionControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float alarmaVolMin, float alarmaVolMax, float PCsobrePEEP){
+PresionControl::PresionControl(float tiempoInspiracion, float triggerFlujo, float frecuencia, float alarmaVolMin, float alarmaVolMax, float PCsobrePEEP):Mandatorio(PEEP,concentracionO2,retardoInspiratorio,complacencia,resistenciaAlveolar,tiempoInspiracion,triggerFlujo,frecuencia){
 	this->alarmaVolMin=alarmaVolMin;
 	this->alarmaVolMax=alarmaVolMax;
 	this->PCsobrePEEP=PCsobrePEEP;
