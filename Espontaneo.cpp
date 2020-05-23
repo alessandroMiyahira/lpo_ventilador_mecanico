0ebefebfd436070110f32ea5c1e993dea179c57d
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-23 16:01:44 -0500

    Update Espontaneo.cpp

--- Espontaneo.cpp
+++ Espontaneo.cpp
@@ -2,7 +2,7 @@
 
 using namespace VentiladorMecanicoModel;
 
-Espontaneo::Espontaneo(float^ PCsobrePEEP, float^ triggerPresion, float^ finalInspiratorio, float^ alarmaVolMax, float^ alarmaVolMin)
+Espontaneo::Espontaneo(float PEEP, float concentracionO2, float retardoInspiratorio, float complasencia, float resistenciaAlveolar, float^ PCsobrePEEP, float^ triggerPresion, float^ finalInspiratorio, float^ alarmaVolMax, float^ alarmaVolMin):ModoVentilatorio(float PEEP, float concentracionO2, float retardoInspiratorio, float complasencia, float resistenciaAlveolar )
 {
 	this->PCsobrePEEP = PCsobrePEEP;
 	this->triggerPresion = triggerPresion;
