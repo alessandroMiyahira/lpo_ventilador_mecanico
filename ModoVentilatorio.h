78067cc70ece6a8cb4e99292f788e104243cfacb
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-23 15:55:28 -0500

    Update ModoVentilatorio.h

--- ModoVentilatorio.h
+++ ModoVentilatorio.h
@@ -7,12 +7,12 @@ namespace VentiladorMecanicoModel
 	using namespace System;
 	public ref class ModoVentilatorio 
 	{
-	private:
-		 float PEEP;
-		 float concentracionO2;
-     float retardoInspiratorio;
-     float complasencia;
-     float resistenciaAlveolar;
+	public:
+		float PEEP;
+		float concentracionO2;
+        	float retardoInspiratorio;
+       	        float complasencia;
+        	float resistenciaAlveolar;
 	public:
 		ModoVentilatorio(float PEEP, float concentracionO2, float retardoInspiratorio, float complasencia, float resistenciaAlveolar );
 	};
