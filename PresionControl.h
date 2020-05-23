6d088f3680c8d8da2205d70d889befadd50840b6
Alessandro Miyahira <65517979+alessandroMiyahira@users.noreply.github.com>
2020-05-23 16:02:52 -0500

    Update PresionControl.h

--- PresionControl.h
+++ PresionControl.h
@@ -14,7 +14,9 @@ namespace VentiladorMecanicoModel{
 		float PCsobrePEEP;
 		
 	public:
-		PresionControl( float alarmaVolMin, float alarmaVolMax, float PCsobrePEEP);
+		PresionControl( float PEEP, float concentracionO2, float retardoInspiratorio, float complasencia, float resistenciaAlveolar, float alarmaVolMin, float alarmaVolMax, float PCsobrePEEP);
 	protected:
 	}	
 }
+
+
